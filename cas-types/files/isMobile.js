11741b824d1998ce9c39cabceb509a41538b7230
Elia Pérennès <elia.prns@gmail.com>
2021-01-07 19:09:01 +0100

    Upload cas types

---
+++ cas-types/files/isMobile.js
@@ -0,0 +1,2 @@
+(function(){define([],function(){return isMobile={Android:function(){return window.navigator.userAgent.match(/Android/i)},BlackBerry:function(){return window.navigator.userAgent.match(/BlackBerry/i)},iOS:function(){return window.navigator.userAgent.match(/iPhone|iPad|iPod/i)},Opera:function(){return window.navigator.userAgent.match(/Opera Mini/i)},Windows:function(){return window.navigator.userAgent.match(/IEMobile/i)},any:function(){return isMobile.Android()||(isMobile.BlackBerry()||(isMobile.iOS()||
+(isMobile.Opera()||isMobile.Windows())))}}})}).call(this);
